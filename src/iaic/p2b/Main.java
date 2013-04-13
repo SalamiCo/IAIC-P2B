c23cd11ad4e7dc11165518e7d66657b303cc7de8
Daniel Escoz <darkhogg@gmail.com>
2013-04-13 19:16:44 +0200

    Unos pocos cambios, arreglados algunos bugs

--- src/iaic/p2b/Main.java
+++ src/iaic/p2b/Main.java
@@ -4,6 +4,7 @@ import iaic.p2b.gui.Interfaz;
 
 import java.util.Iterator;
 
+import javax.swing.SwingUtilities;
 import javax.swing.table.DefaultTableModel;
 
 import jess.Deffacts;
@@ -14,126 +15,123 @@ import jess.Rete;
 import jess.Value;
 
 public class Main {
-
-	private static Rete miRete;
-	
-	//Nombre del archivo .clp que contiene el código Jess
-	private static String archivoJess;
-	
-	public static void cargaPrograma(String nombre) {
-		try {
-			Value v = miRete.batch(nombre);
-
-			System.out.println("Value " + v);
-		} catch (JessException je0) {
-			System.out.println("Error de lectura en " + nombre);
-			je0.printStackTrace();
-		}
-	}
-	
-	public static void run(){
-		try{
-			miRete.run();
-		} catch (JessException je) {
-			System.out.println("Error al ejecutar el modulo");
-			je.printStackTrace();
-		}
-	}
-	
-	public static void reset(){
-		try{
-			miRete.reset();
-		} catch (JessException je) {
-			System.out.println("Error al hacer reset");
-			je.printStackTrace();
-		}
-	}
-	
-	public static void halt(){
-		try{
-			miRete.halt();
-		} catch (JessException je) {
-			System.out.println("Error al hacer halt");
-			je.printStackTrace();
-		}
-	}
-	
-	public static void citar(Persona persona) throws JessException {
-		Deffacts deffacts = new Deffacts("usuarios", null, miRete);
-		Fact f = new Fact("persona", miRete);
-		f.setSlotValue("nombre", new Value(persona.getNombre(), RU.SYMBOL));
-		f.setSlotValue("sexo", new Value(persona.getSexo(), RU.SYMBOL));
-		f.setSlotValue("edad", new Value(persona.getEdad(), RU.INTEGER));
-		f.setSlotValue("altura", new Value(persona.getAltura(), RU.INTEGER));
-		f.setSlotValue("peso", new Value(persona.getPeso(), RU.INTEGER));
-		f.setSlotValue("cuestaHablar", new Value(persona.getCuestaHablar(), RU.SYMBOL));
-		f.setSlotValue("gustaSalir", new Value(persona.getGustaSalir(), RU.SYMBOL));
-		f.setSlotValue("numAmigos", new Value(persona.getNumAmigos(), RU.INTEGER));
-		f.setSlotValue("religion", new Value(persona.getReligion(), RU.SYMBOL));
-		f.setSlotValue("twitter", new Value(persona.getTwitter(), RU.SYMBOL));
-		f.setSlotValue("facebook", new Value(persona.getFacebook(), RU.SYMBOL));
-		deffacts.addFact(f);
-		miRete.addDeffacts(deffacts);
-		
-		reset();
-		
-		miRete.setFocus("clasificar");
-		run();
-		miRete.setFocus("compatibilizar");
-		run();
-		miRete.setFocus("citar");
-		run();
-		miRete.setFocus("mostrar");
-		run();
-		
-		halt();
-	}
-	
-	@SuppressWarnings("rawtypes")
-	public static DefaultTableModel mostrarCitas(Persona persona) throws JessException {
-		DefaultTableModel modelo = new DefaultTableModel();
-		modelo.addColumn("Nombre");
-		modelo.addColumn("Tipo de cita");
-		
-		Object [] datosFila = new Object[2];
-		
-		Iterator it = miRete.listFacts();
-		while (it.hasNext()) {
-			Fact f = (Fact) it.next();
-			if(f.getName().equals("MAIN::emparejamiento")){
-				String tipoCita = "";
-				if(f.getSlotValue("tipoCita").stringValue(miRete.getGlobalContext()).equals("sinCita")){
-					tipoCita = "Compatibles";
-				} else if (f.getSlotValue("tipoCita").stringValue(miRete.getGlobalContext()).equals("citaNormal")){
-					tipoCita = "Cita normal";
-				} else if(f.getSlotValue("tipoCita").stringValue(miRete.getGlobalContext()).equals("citaMagica")){
-					tipoCita = "Cita mágica";
-				}
-				
-				String nombre1 = f.getSlotValue("nombre1").stringValue(miRete.getGlobalContext());
-				String nombre2 = f.getSlotValue("nombre2").stringValue(miRete.getGlobalContext());
-				
-				if (nombre1.equals(persona.getNombre())){
-					datosFila[0] = nombre2;
-					datosFila[1] = tipoCita;
-					modelo.addRow(datosFila);
-				} else if (nombre2.equals(persona.getNombre())){
-					datosFila[0] = nombre1;
-					datosFila[1] = tipoCita;
-					modelo.addRow(datosFila);
-				}
-			}
-		}
-		return modelo;
-	}
-	
-	public static void main(String[] args) {
-		miRete = new Rete();
-		archivoJess = "jess.clp";
-		cargaPrograma(archivoJess);
-
-		Interfaz interfaz = new Interfaz();
-		interfaz.setVisible(true);
-	}
-
+    
+    private static Rete miRete;
+    
+    // Nombre del archivo .clp que contiene el cï¿½digo Jess
+    private static String archivoJess;
+    
+    public static void cargaPrograma (final String nombre) {
+        try {
+            Value v = miRete.batch(nombre);
+            
+            System.out.println("Value " + v);
+        } catch (JessException je0) {
+            System.out.println("Error de lectura en " + nombre);
+            je0.printStackTrace();
+        }
+    }
+    
+    public static void citar (final Persona persona) throws JessException {
+        // Comprobamos que usuario no existe ya
+        Iterator<Fact> it = miRete.listFacts();
+        while (it.hasNext()) {
+            Fact f = it.next();
+            if (f.getName().equals("MAIN::persona") && f.getSlotValue("nombre").equals(persona.getNombre())) {
+                throw new RuntimeException("Ya existe ese usuario en la BdC");
+            }
+        }
+        
+        Deffacts deffacts = new Deffacts("usuarios", null, miRete);
+        Fact f = new Fact("persona", miRete);
+        f.setSlotValue("nombre", new Value(persona.getNombre(), RU.SYMBOL));
+        f.setSlotValue("sexo", new Value(persona.getSexo(), RU.SYMBOL));
+        f.setSlotValue("edad", new Value(persona.getEdad(), RU.INTEGER));
+        f.setSlotValue("altura", new Value(persona.getAltura(), RU.INTEGER));
+        f.setSlotValue("peso", new Value(persona.getPeso(), RU.INTEGER));
+        f.setSlotValue("cuestaHablar", new Value(persona.getCuestaHablar(), RU.SYMBOL));
+        f.setSlotValue("gustaSalir", new Value(persona.getGustaSalir(), RU.SYMBOL));
+        f.setSlotValue("numAmigos", new Value(persona.getNumAmigos(), RU.INTEGER));
+        f.setSlotValue("religion", new Value(persona.getReligion(), RU.SYMBOL));
+        f.setSlotValue("twitter", new Value(persona.getTwitter(), RU.SYMBOL));
+        f.setSlotValue("facebook", new Value(persona.getFacebook(), RU.SYMBOL));
+        deffacts.addFact(f);
+        miRete.addDeffacts(deffacts);
+        
+        miRete.reset();
+        
+        miRete.setFocus("clasificar");
+        miRete.run();
+        
+        miRete.setFocus("compatibilizar");
+        miRete.run();
+        
+        miRete.setFocus("citar");
+        miRete.run();
+        
+        // DepuraciÃ³n
+        miRete.setFocus("mostrar");
+        miRete.run();
+        
+        miRete.halt();
+    }
+    
+    public static DefaultTableModel mostrarCitas (final Persona persona) throws JessException {
+        DefaultTableModel modelo = new DefaultTableModel();
+        modelo.addColumn("Nombre");
+        modelo.addColumn("Tipo de cita");
+        
+        Object[] datosFila = new Object[2];
+        
+        @SuppressWarnings("unchecked")
+        Iterator<Fact> it = miRete.listFacts();
+        
+        while (it.hasNext()) {
+            Fact f = it.next();
+            if (f.getName().equals("MAIN::emparejamiento")) {
+                
+                String tipoCita = "?";
+                if (f.getSlotValue("tipoCita").stringValue(miRete.getGlobalContext()).equals("sinCita")) {
+                    tipoCita = "Compatibles";
+                } else if (f.getSlotValue("tipoCita").stringValue(miRete.getGlobalContext()).equals("citaNormal")) {
+                    tipoCita = "Cita normal";
+                } else if (f.getSlotValue("tipoCita").stringValue(miRete.getGlobalContext()).equals("citaMagica")) {
+                    tipoCita = "Cita mï¿½gica";
+                }
+                
+                String nombre1 = f.getSlotValue("nombre1").stringValue(miRete.getGlobalContext());
+                String nombre2 = f.getSlotValue("nombre2").stringValue(miRete.getGlobalContext());
+                
+                if (nombre1.equals(persona.getNombre())) {
+                    datosFila[0] = nombre2;
+                    datosFila[1] = tipoCita;
+                    modelo.addRow(datosFila);
+                    
+                } else if (nombre2.equals(persona.getNombre())) {
+                    datosFila[0] = nombre1;
+                    datosFila[1] = tipoCita;
+                    modelo.addRow(datosFila);
+                }
+            }
+        }
+        
+        return modelo;
+    }
+    
+    public static void main (final String[] args) {
+        miRete = new Rete();
+        archivoJess = "jess.clp";
+        cargaPrograma(archivoJess);
+        
+        SwingUtilities.invokeLater(new Runnable() {
+            
+            @Override
+            public void run () {
+                Interfaz interfaz = new Interfaz();
+                interfaz.setVisible(true);
+            }
+            
+        });
+    }
+    
 }
--- src/iaic/p2b/gui/Interfaz.java
+++ src/iaic/p2b/gui/Interfaz.java
@@ -7,20 +7,22 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
-
-import jess.JessException;
 
 import com.jgoodies.forms.factories.FormFactory;
 import com.jgoodies.forms.layout.ColumnSpec;
@@ -29,191 +31,232 @@ import com.jgoodies.forms.layout.RowSpec;
 
 @SuppressWarnings("serial")
 public class Interfaz extends JFrame {
-
-	private JPanel contentPane;
-	private JLabel labelNombre;
-	private JLabel labelSexo;
-	private JLabel labelEdad;
-	private JLabel labelAltura;
-	private JLabel labelPeso;
-	private JLabel labelNumAmigos;
-	private JLabel labelReligion;
-	private JTextField textFieldNombre;
-	private JTextField textFieldEdad;
-	private JTextField textFieldAltura;
-	private JTextField textFieldPeso;
-	private JTextField textFieldNumAmigos;
-	private JButton botonCitar;
-	private JCheckBox checkboxCuestaHablar;
-	private JCheckBox checkboxGustaSalir;
-	private JCheckBox checkboxTwitter;
-	private JCheckBox checkboxFacebook;
-	private JScrollPane scrollPane;
-	private JComboBox<String> comboBoxSexo;
-	private JComboBox<String> comboBoxReligion;
-	private JTable table;
-	private DefaultTableModel modelo;
-
-	
-	private Persona persona;
-	
-	/**
-	 * Create the frame.
-	 */
-	public Interfaz() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle("IAIC-P2B");
-		setBounds(100, 100, 502, 389);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(new BorderLayout(0, 0));
-		
-		JPanel panel = new JPanel();
-		contentPane.add(panel, BorderLayout.WEST);
-		contentPane.setPreferredSize(new Dimension(480, 380));
-		panel.setLayout(new FormLayout(new ColumnSpec[] {
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,
-				FormFactory.RELATED_GAP_COLSPEC,
-				ColumnSpec.decode("default:grow"),},
-			new RowSpec[] {
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,}));
-		
-		labelNombre = new JLabel("Nombre");
-		panel.add(labelNombre, "2, 2, right, default");
-		
-		textFieldNombre = new JTextField();
-		panel.add(textFieldNombre, "4, 2, fill, default");
-		textFieldNombre.setColumns(10);
-		
-		labelSexo = new JLabel("Sexo");
-		panel.add(labelSexo, "2, 4, right, default");
-		
-		String[] arraySexo = {"hombre","mujer"};
-		comboBoxSexo = new JComboBox<String>(arraySexo);
-		panel.add(comboBoxSexo, "4, 4, fill, default");
-		
-		labelEdad = new JLabel("Edad");
-		panel.add(labelEdad, "2, 6, right, default");
-		
-		textFieldEdad = new JTextField();
-		panel.add(textFieldEdad, "4, 6, fill, default");
-		textFieldEdad.setColumns(10);
-		
-		labelAltura = new JLabel("Altura");
-		panel.add(labelAltura, "2, 8, right, default");
-		
-		textFieldAltura = new JTextField();
-		panel.add(textFieldAltura, "4, 8, fill, default");
-		textFieldAltura.setColumns(10);
-		
-		labelPeso = new JLabel("Peso");
-		panel.add(labelPeso, "2, 10, right, default");
-		
-		textFieldPeso = new JTextField();
-		panel.add(textFieldPeso, "4, 10, fill, default");
-		textFieldPeso.setColumns(10);
-		
-		labelNumAmigos = new JLabel("N\u00BA Amigos");
-		panel.add(labelNumAmigos, "2, 12, right, default");
-		
-		textFieldNumAmigos = new JTextField();
-		panel.add(textFieldNumAmigos, "4, 12, fill, default");
-		textFieldNumAmigos.setColumns(10);
-		
-		labelReligion = new JLabel("Religi\u00F3n");
-		panel.add(labelReligion, "2, 14, right, default");
-		
-		String[] arrayReligion = {"ateo", "cristiano", "judio"};
-		comboBoxReligion = new JComboBox<String>(arrayReligion);
-		panel.add(comboBoxReligion, "4, 14, fill, default");
-		
-		checkboxCuestaHablar = new JCheckBox("\u00BFTe cuesta hablar?");
-		panel.add(checkboxCuestaHablar, "2, 16, 3, 1");
-		
-		checkboxGustaSalir = new JCheckBox("\u00BFTe gusta salir?");
-		panel.add(checkboxGustaSalir, "2, 18, 3, 1");
-		
-		checkboxTwitter = new JCheckBox("\u00BFTienes Twitter?");
-		panel.add(checkboxTwitter, "2, 20, 3, 1");
-		
-		checkboxFacebook = new JCheckBox("\u00BFTienes Facebook?");
-		panel.add(checkboxFacebook, "2, 22, 3, 1");
-		
-		botonCitar = new JButton("Citar");
-		panel.add(botonCitar, "2, 24, 3, 1");
-		
-		scrollPane = new JScrollPane();
-		contentPane.add(scrollPane, BorderLayout.EAST);
-		scrollPane.setPreferredSize(new Dimension(250, 300));
-		
-		table = new JTable();
-		scrollPane.setViewportView(table);
-		modelo = new DefaultTableModel();
-		modelo.addColumn("Nombre");
-		modelo.addColumn("Tipo de cita");
-		table.setModel(modelo);
-		
-		botonCitar.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				String nombre = textFieldNombre.getText();
-				String sexo = comboBoxSexo.getSelectedItem().toString();
-				int edad = Integer.parseInt(textFieldEdad.getText());
-				int altura = Integer.parseInt(textFieldAltura.getText());
-				int peso = Integer.parseInt(textFieldPeso.getText());
-				int numAmigos = Integer.parseInt(textFieldNumAmigos.getText());
-				String religion = comboBoxReligion.getSelectedItem().toString();
-				String cuestaHablar = (checkboxCuestaHablar.isSelected() ? "TRUE" : "FALSE" );
-				String gustaSalir = (checkboxGustaSalir.isSelected() ? "TRUE" : "FALSE" );
-				String twitter = (checkboxTwitter.isSelected() ? "TRUE" : "FALSE" );
-				String facebook = (checkboxFacebook.isSelected() ? "TRUE" : "FALSE" );
-				
-				persona = new Persona(nombre, sexo, edad, altura, peso, cuestaHablar, gustaSalir, numAmigos, religion, twitter, facebook);
-				
-				try {
-					Main.citar(persona);
-				} catch (JessException e) {
-					System.out.println("Error al citar");
-					e.printStackTrace();
-				}
-				
-				try {
-					table.setModel(Main.mostrarCitas(persona));
-				} catch (JessException e) {
-					System.out.println("Error al mostrar citas");
-					e.printStackTrace();
-				}
-			}
-		});
-		
-		pack();
-	}
-	
+    
+    private JPanel contentPane;
+    
+    private JLabel labelNombre;
+    
+    private JLabel labelSexo;
+    
+    private JLabel labelEdad;
+    
+    private JLabel labelAltura;
+    
+    private JLabel labelPeso;
+    
+    private JLabel labelNumAmigos;
+    
+    private JLabel labelReligion;
+    
+    private JTextField textFieldNombre;
+    
+    private JTextField textFieldEdad;
+    
+    private JTextField textFieldAltura;
+    
+    private JTextField textFieldPeso;
+    
+    private JTextField textFieldNumAmigos;
+    
+    private JButton botonCitar;
+    
+    private JCheckBox checkboxCuestaHablar;
+    
+    private JCheckBox checkboxGustaSalir;
+    
+    private JCheckBox checkboxTwitter;
+    
+    private JCheckBox checkboxFacebook;
+    
+    private JScrollPane scrollPane;
+    
+    private JComboBox<String> comboBoxSexo;
+    
+    private JComboBox<String> comboBoxReligion;
+    
+    private JTable table;
+    
+    private DefaultTableModel modelo;
+    
+    private Persona persona;
+    
+    /**
+     * Create the frame.
+     */
+    public Interfaz () {
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("IAIC-P2B");
+        setBounds(100, 100, 502, 389);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(new BorderLayout(0, 0));
+        
+        JPanel panel = new JPanel();
+        contentPane.add(panel, BorderLayout.WEST);
+        contentPane.setPreferredSize(new Dimension(480, 380));
+        panel.setLayout(new FormLayout(new ColumnSpec[] {
+            FormFactory.RELATED_GAP_COLSPEC, FormFactory.DEFAULT_COLSPEC, FormFactory.RELATED_GAP_COLSPEC,
+            ColumnSpec.decode("default:grow"),}, new RowSpec[] {
+            FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC,
+            FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC,
+            FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC,
+            FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC,
+            FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC,
+            FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC,
+            FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC,
+            FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC,
+            FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC,}));
+        
+        labelNombre = new JLabel("Nombre");
+        panel.add(labelNombre, "2, 2, right, default");
+        
+        textFieldNombre = new JTextField();
+        panel.add(textFieldNombre, "4, 2, fill, default");
+        textFieldNombre.setColumns(10);
+        
+        labelSexo = new JLabel("Sexo");
+        panel.add(labelSexo, "2, 4, right, default");
+        
+        String[] arraySexo = {"hombre", "mujer"};
+        comboBoxSexo = new JComboBox<String>(arraySexo);
+        panel.add(comboBoxSexo, "4, 4, fill, default");
+        
+        labelEdad = new JLabel("Edad");
+        panel.add(labelEdad, "2, 6, right, default");
+        
+        textFieldEdad = new JTextField();
+        panel.add(textFieldEdad, "4, 6, fill, default");
+        textFieldEdad.setColumns(10);
+        
+        labelAltura = new JLabel("Altura");
+        panel.add(labelAltura, "2, 8, right, default");
+        
+        textFieldAltura = new JTextField();
+        panel.add(textFieldAltura, "4, 8, fill, default");
+        textFieldAltura.setColumns(10);
+        
+        labelPeso = new JLabel("Peso");
+        panel.add(labelPeso, "2, 10, right, default");
+        
+        textFieldPeso = new JTextField();
+        panel.add(textFieldPeso, "4, 10, fill, default");
+        textFieldPeso.setColumns(10);
+        
+        labelNumAmigos = new JLabel("N\u00BA Amigos");
+        panel.add(labelNumAmigos, "2, 12, right, default");
+        
+        textFieldNumAmigos = new JTextField();
+        panel.add(textFieldNumAmigos, "4, 12, fill, default");
+        textFieldNumAmigos.setColumns(10);
+        
+        labelReligion = new JLabel("Religi\u00F3n");
+        panel.add(labelReligion, "2, 14, right, default");
+        
+        String[] arrayReligion = {"ateo", "cristiano", "judio"};
+        comboBoxReligion = new JComboBox<String>(arrayReligion);
+        panel.add(comboBoxReligion, "4, 14, fill, default");
+        
+        checkboxCuestaHablar = new JCheckBox("\u00BFTe cuesta hablar?");
+        panel.add(checkboxCuestaHablar, "2, 16, 3, 1");
+        
+        checkboxGustaSalir = new JCheckBox("\u00BFTe gusta salir?");
+        panel.add(checkboxGustaSalir, "2, 18, 3, 1");
+        
+        checkboxTwitter = new JCheckBox("\u00BFTienes Twitter?");
+        panel.add(checkboxTwitter, "2, 20, 3, 1");
+        
+        checkboxFacebook = new JCheckBox("\u00BFTienes Facebook?");
+        panel.add(checkboxFacebook, "2, 22, 3, 1");
+        
+        botonCitar = new JButton("Citar");
+        panel.add(botonCitar, "2, 24, 3, 1");
+        
+        scrollPane = new JScrollPane();
+        contentPane.add(scrollPane, BorderLayout.EAST);
+        scrollPane.setPreferredSize(new Dimension(250, 300));
+        
+        table = new JTable();
+        scrollPane.setViewportView(table);
+        modelo = new DefaultTableModel();
+        modelo.addColumn("Nombre");
+        modelo.addColumn("Tipo de cita");
+        table.setModel(modelo);
+        
+        botonCitar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed (final ActionEvent arg0) {
+                if (validarCampos()) {
+                    
+                    String nombre = textFieldNombre.getText();
+                    String sexo = comboBoxSexo.getSelectedItem().toString();
+                    int edad = Integer.parseInt(textFieldEdad.getText());
+                    int altura = Integer.parseInt(textFieldAltura.getText());
+                    int peso = Integer.parseInt(textFieldPeso.getText());
+                    int numAmigos = Integer.parseInt(textFieldNumAmigos.getText());
+                    String religion = comboBoxReligion.getSelectedItem().toString();
+                    String cuestaHablar = (checkboxCuestaHablar.isSelected() ? "TRUE" : "FALSE");
+                    String gustaSalir = (checkboxGustaSalir.isSelected() ? "TRUE" : "FALSE");
+                    String twitter = (checkboxTwitter.isSelected() ? "TRUE" : "FALSE");
+                    String facebook = (checkboxFacebook.isSelected() ? "TRUE" : "FALSE");
+                    
+                    persona =
+                        new Persona(
+                            nombre, sexo, edad, altura, peso, cuestaHablar, gustaSalir, numAmigos, religion, twitter,
+                            facebook);
+                    
+                    try {
+                        Main.citar(persona);
+                        table.setModel(Main.mostrarCitas(persona));
+                        
+                    } catch (Exception e) {
+                        JOptionPane.showMessageDialog(
+                            Interfaz.this, "Error al citar: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        
+        pack();
+    }
+    
+    private boolean validarCampos () {
+        boolean correct = true;
+        List<String> fields = new ArrayList<String>(4);
+        
+        if (!textFieldEdad.getText().matches("\\d+")) {
+            correct = false;
+            fields.add("edad");
+        }
+        if (!textFieldAltura.getText().matches("\\d+")) {
+            correct = false;
+            fields.add("altura");
+        }
+        if (!textFieldPeso.getText().matches("\\d+")) {
+            correct = false;
+            fields.add("peso");
+        }
+        if (!textFieldNumAmigos.getText().matches("\\d+")) {
+            correct = false;
+            fields.add("amigos");
+        }
+        
+        if (!correct) {
+            StringBuilder sb = new StringBuilder("Los siguientes campos son inválidos: ");
+            
+            boolean init = true;
+            for (String field : fields) {
+                if (!init) {
+                    sb.append(", ");
+                } else {
+                    init = false;
+                }
+                sb.append(field);
+            }
+            JOptionPane.showMessageDialog(this, sb.toString(), "Error de formato", JOptionPane.ERROR_MESSAGE);
+        }
+        
+        return correct;
+    }
 }
